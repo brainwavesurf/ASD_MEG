5c411d9e4a4d7868fecb1edf7788b97552ab38d5
Anna Shishkina <shishki.ann@gmail.com>
2019-09-30 22:52:51 +0300

    paired t-test and cluster-based permutation test for data after mtmfft for the averaged alpha frequency on 306 channels

--- my_scripts/stat_between_subj.m
+++ my_scripts/stat_between_subj.m
@@ -1,20 +1,18 @@
 %the probability distribution of the condition-specific averages is identical for all experimental conditions
+clear all;
+close all;
+
+fieldtripfolder = '/home/a_shishkina/fieldtrip/';
+path(fieldtripfolder, path);
+ft_defaults;
+path('/home/a_shishkina/fieldtrip/external/mne/', path);
+realdatapath = '/home/a_shishkina/data/KI/SUBJECTS/';
+savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
 %%
 clear all;
 close all;
 %loading data with powers
-SUBJ = ['0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139'; '0140'];
-% posterior sensors, you may use them later for extracting alpha peak power
-post_ch = {'MEG1932',  'MEG1922', 'MEG2042',  'MEG2032',  'MEG2112', 'MEG2122',  'MEG2342', 'MEG2332',  'MEG1732', 'MEG1942', 'MEG1912', 'MEG2012', 'MEG2022', 'MEG2312', 'MEG2322', 'MEG2512',...
-%           'MEG1933',  'MEG1923', 'MEG2043',  'MEG2033',  'MEG2113', 'MEG2123',  'MEG2343', 'MEG2333',  'MEG1733', 'MEG1943', 'MEG1913', 'MEG2013', 'MEG2023', 'MEG2313', 'MEG2323', 'MEG2513'};
-
-%% grand averaging
-cfg = [],
-tfr_avg_ = ft_freqgrandaverage([], tfr_slow);
-
-
-cfg = [];
-tfr_app = ft_appendfreq(cfg, tfr_slow, tfr_fast);
+SUBJ = ['0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139'; '0140'; '0141'; '0158'; '0159'; '0160'; '0160'; '0161'; '0162'; '0163'; '0164'; '0178'; '0179'; '0253'; '0254'; '0255'; '0256'; '0257'; '0259'; '0273'; '0274'; '0275'; '0276'; '0277'; '0346'; '0347'; '0348'; '0350'; '0351'; '0357'; '0358'; '0378'; '0380'; '0381'; '0382'; '0383'; '0384']; 
 
 for s = 1: size (SUBJ,1)
     close all
@@ -22,32 +20,75 @@ for s = 1: size (SUBJ,1)
     savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
     allpower = load(strcat(savepath, subj, '/', subj, '_alpha_power.mat'));
     
-    
-    %allsubj_slow{s} = allpower.powPre{1};
-    %allsubj_fast{s} = allpower.powPre{3};
-    
-    %[h,p] = ttest(allsubj_slow{s}, allsubj_fast{s});
-    %p_val{s} = p; 
-    
+    pow_slow{s} = allpower.freqPre{1};
+    pow_fast{s} = allpower.freqPre{3};
    
 end
+%% Calculate paired t-test for slow and fast conditions
 
-
-tfr_slow(s) = allpower.freqPre{1};
-tfr_fast(s) = allpower.freqPre{3};
-
-
- cfg             = [];
+cfg             = [];
 cfg.method      = 'stats'; % using a parametric test
-cfg.statistic   = 'depsamplesT'; % using dependent samples
+cfg.statistic   = 'paired-ttest'; % using dependent samples
 cfg.correctm    = 'fdr'; % no multiple comparisons correction
 cfg.alpha       = 0.05;
 
-nsubj           = numel(size (SUBJ,1));
+nsubj           = (size (SUBJ,1));
+%cfg.design(1,:) = [1:nsubj 1:nsubj];
+cfg.design(1,:) = [ones(1,nsubj)*1 ones(1,nsubj)*2];
+%cfg.uvar        = 1; % row of design matrix that contains unit variable (in this case: subjects)
+cfg.ivar        = 1; % row of design matrix that contains independent variable (the conditions)
+
+paired_ttest = ft_freqstatistics(cfg, pow_slow{:}, pow_fast{:});
+
+
+%% Calculate cluster based permutation test
+cfg = [];
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan        = 2;
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 500;
+% prepare_neighbours determines what sensors may form clusters
+cfg_neighb.method    = 'distance';
+cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, pow_slow{1});
+
+nsubj           = (size (SUBJ,1));
 cfg.design(1,:) = [1:nsubj 1:nsubj];
 cfg.design(2,:) = [ones(1,nsubj)*1 ones(1,nsubj)*2];
 cfg.uvar        = 1; % row of design matrix that contains unit variable (in this case: subjects)
 cfg.ivar        = 2; % row of design matrix that contains independent variable (the conditions)
 
-stat{s} = ft_freqstatistics(cfg, allpower.freqPre{1}, allpower.freqPre{3});
+[cluster_stat] = ft_freqstatistics(cfg, pow_slow{:}, pow_fast{:});
 
+% calculate the grand average for each condition
+cfg = [];
+GA_slow       = ft_freqgrandaverage(cfg,pow_slow{:});
+GA_fast       = ft_freqgrandaverage(cfg,pow_fast{:});
+% "{:}" means to use data from all elements of the variable
+
+cfg = [];
+cfg.operation = 'subtract';
+cfg.parameter = 'powspctrm';
+GA_fastVSslow = ft_math(cfg,GA_fast,GA_slow);
+
+% In case you have downloaded and loaded the data, ensure stat.cfg.alpha exist
+if ~isfield(cluster_stat.cfg,'alpha'); cluster_stat.cfg.alpha = 0.025; end; % stat.cfg.alpha was moved as the downloaded data was processed by an additional FieldTrip function to anonymize the data.
+
+% plot
+figure(1);
+pos_int = zeros(numel(GA_fastVSslow.label),1);
+cfg.highlight = 'on';
+cfg.highlightchannel = find(pos_int);
+cfg.commentpos = 'title';
+cfg.layout = 'neuromag306planar_helmet.mat';
+ft_topoplotER(cfg, GA_fastVSslow);
+
+saveas(figure(1),[savepath, '1_results_plot/', 'cluster_based_PSD.jpeg']);
+
+filename = strcat(savepath, '1_results_plot/', 'paired_ttest.mat');
+save (filename, 'paired_ttest', 'cluster_stat');
