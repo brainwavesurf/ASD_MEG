0124abf20487dda4bb6d07e0c04fddeca6acd54e
Anna Shishkina <shishki.ann@gmail.com>
2019-10-03 17:31:35 +0300

    Cluster-based permutation test for within-subjects experiments

--- my_scripts/cluster_based_permut_test.m
+++ my_scripts/cluster_based_permut_test.m
@@ -1,9 +1,17 @@
-%% Cluster-based permutation test fo between-trial experiments (two cond)
-% Reading the data for slow and fast conditions
-% Calculation of time-frequency analysis
-% Permutation test (posclusters: [1×2 struct], negclusters: [1×9 struct])
-% Plotting the results      
-% no clusters present with a p-value lower than the specified alpha, nothing to plot
+%% Cluster-based permutation test for within-subjects experiments 
+%(http://www.fieldtriptoolbox.org/tutorial/cluster_permutation_timelock/#within-subjects-experiments)
+% Reading the data for trials in slow and fast conditions
+% Do time-frequency transformation with wavelets for [-1 1.2] time interval
+% Do the grand averages of the TFRs for the fast and slow conditions
+% Do group-level stats for interval of interest [-0.8 0] and frequency range [5 20]
+%==========================================================================
+% error!!!   
+%Error using ft_statfun_depsamplesT (line 84)
+%The data must contain at least two units (usually subjects).
+%Error using ft_statistics_montecarlo (line 244)
+%could not determine the parametric critical value for clustering
+%Error in ft_freqstatistics (line 193)
+%[stat, cfg] = statmethod(cfg, dat, design);
 %%
 clear all;
 close all;
@@ -18,8 +26,8 @@ savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
 
 %%
 %add list of subjects:
-SUBJ = ['0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139'; '0140'; '0141'; '0158'; '0159'; '0160'; '0161'; '0162'; '0163'; '0164'; '0178'; '0179'; '0253'; '0254'; '0255'; '0256'; '0257'; '0259'; '0273'; '0274'; '0275'; '0276'; '0277'; '0346'; '0347'; '0348'; '0350'; '0351'; '0357'; '0358'; '0378'; '0380'; '0381'; '0382'; '0383'; '0384'; '0385']; 
-%SUBJ = ['0076'];
+%SUBJ = ['0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139'; '0140'; '0141'; '0158'; '0159'; '0160'; '0161'; '0162'; '0163'; '0164'; '0178'; '0179'; '0253'; '0254'; '0255'; '0256'; '0257'; '0259'; '0273'; '0274'; '0275'; '0276'; '0277'; '0346'; '0347'; '0348'; '0350'; '0351'; '0357'; '0358'; '0378'; '0380'; '0381'; '0382'; '0383'; '0384'; '0385']; 
+SUBJ = ['0102';'0103'; '0104'];
 
 for s=1: size (SUBJ,1)
     close all
@@ -33,13 +41,13 @@ for s=1: size (SUBJ,1)
     slow_ind = find(allinfo.prev_stim_type==2);
     fast_ind = find(allinfo.prev_stim_type==8);
  
-    %% Load unfiltered epochs. If you need to filter the data (e.g. for LCMV), import raw, not epochs.
+    %% Load unfiltered epochs 
     ep_fiff_file = strcat(epofolder, subj, '-noerror-lagcorrected-epo.fif')
     hdr = ft_read_header(ep_fiff_file);
     
     cfg           = [];  
     cfg.dataset   = ep_fiff_file;
-    cfg.channel   = {'MEGGRAD'};
+    cfg.channel   = {'MEG'};
     epochs        = ft_preprocessing(cfg);
     
     cfg           = [];
@@ -49,75 +57,76 @@ for s=1: size (SUBJ,1)
     cfg.trials    = fast_ind;
     slow_epochs   = ft_selectdata(cfg, epochs);
     
-    %% Calculate the TFRs for the two experimental conditions (Fast and Slow).
+    %% Calculate the TFRs for the two experimental conditions (Fast and Slow) for each subject
     cfg = [];
-    cfg.output     = 'pow';
     cfg.channel    = 'MEG';
-    cfg.method     = 'mtmconvol';
-    cfg.taper      = 'hanning';
-    cfg.foi        = [7:20];
-    cfg.toi        = [-0.8:0.05:0];
-    cfg.t_ftimwin  = 7./cfg.foi; %7 cycles
-    cfg.keeptrials = 'yes';
+    cfg.method     = 'wavelet';
+    cfg.width      = 7;
+    cfg.output     = 'pow';
+    cfg.foi        = 5:20;
+    cfg.toi        = -1:0.05:1.2;
 
-    freqFast  = ft_freqanalysis(cfg, fast_epochs);
-    freqSlow  = ft_freqanalysis(cfg, slow_epochs);
-
-    %% do permutation test
-    cfg = [];
-    cfg.channel          = {'MEG'};
-    cfg.latency          = 'all';
-    cfg.frequency        = [7 14];
-    cfg.method           = 'montecarlo';
-    cfg.statistic        = 'ft_statfun_indepsamplesT';
-    cfg.correctm         = 'cluster';
-    cfg.clusteralpha     = 0.05;
-    cfg.clusterstatistic = 'maxsum';
-    cfg.minnbchan        = 2;
-    cfg.tail             = 0;
-    cfg.clustertail      = 0;
-    cfg.alpha            = 0.025;
-    cfg.numrandomization = 500;
-    % prepare_neighbours determines what sensors may form clusters
-    cfg_neighb.method    = 'distance';
-    cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, fast_epochs);
-
-    design = zeros(1,size(freqSlow.powspctrm,1) + size(freqFast.powspctrm,1));
-    design(1,1:size(freqSlow.powspctrm,1)) = 1;
-    design(1,(size(freqSlow.powspctrm,1)+1):(size(freqSlow.powspctrm,1)+...
-    size(freqFast.powspctrm,1))) = 2;
-
-    cfg.design           = design;
-    cfg.ivar             = 1;
-
-    [stat] = ft_freqstatistics(cfg, freqSlow, freqFast);
+    freqFast{s}  = ft_freqanalysis(cfg, fast_epochs);
+    freqSlow{s}  = ft_freqanalysis(cfg, slow_epochs);
     
-    filename = strcat(savemegto, '/', subj, '_cluster_based_permut_sensors.mat');
-    save (filename, 'stat');
     
-    % Averaging over epochs
-    cfg = [];
-    cfg.avgoverfreq = 'yes';
-    stat_avg = ft_selectdata(cfg, stat);
-    
-    %% Plotting the results
-    
-    cfg = [];
-    freqSlow = ft_freqdescriptives(cfg, freqSlow);
-    freqFast = ft_freqdescriptives(cfg, freqFast);
-    
-    cfg = [];
-    cfg.avgoverfreq = 'yes';
-    freqSlow_avg = ft_selectdata(cfg, freqSlow);
-    freqFast_avg = ft_selectdata(cfg, freqFast);
-    
-    stat_avg.raweffect = freqFast_avg.powspctrm - freqSlow_avg.powspctrm;
-    
-    cfg = [];
-    cfg.alpha  = 0.025;
-    cfg.parameter = 'raweffect';
-    cfg.zlim   = [-1e-27 1e-27];
-    cfg.layout = 'neuromag306planar_helmet.mat';
-    ft_clusterplot(cfg, stat_avg);
-
+    %Plot the results
+    %cfg = [];
+    %cfg.showlabels   = 'yes';
+    %cfg.layout       = 'neuromag306planar_helmet.mat';
+    %figure
+    %ft_multiplotTFR(cfg, freqFast)
 end
+%% Calculate the grand averages of the TFRs for the fast and slow conditions
+
+cfg = [];
+cfg.foilim = [5 20];
+cfg.toilim = [-0.8 0];
+cfg.channel = 'meg';
+slow_avg = ft_freqgrandaverage(cfg, freqSlow{:});
+fast_avg = ft_freqgrandaverage(cfg, freqFast{:});
+
+%Averaging of grads info because of warning "discarding gradiometer information because it cannot be averaged" 
+AvgGrad_fast = ft_average_sens([freqFast{1}.grad, freqFast{2}.grad, freqFast{3}.grad]);
+fast_avg.grad = AvgGrad_fast;
+
+AvgGrad_slow = ft_average_sens([freqSlow{1}.grad, freqSlow{2}.grad, freqSlow{3}.grad]);
+slow_avg.grad = AvgGrad_slow;
+
+%% do cluster-based permutation test
+cfg = [];
+cfg.channel          = {'MEG'};
+cfg.latency          = [-0.8 0];
+cfg.frequency        = [5 20];
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan        = 2;
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 500;
+% specifies with which sensors other sensors can form clusters
+cfg_neighb.method    = 'distance';
+cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, fast_avg); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
+
+subj = size (SUBJ,1);
+design = zeros(2,2*subj);
+design(1,1:subj)        = 1;
+design(1,subj+1:2*subj) = 2;
+for i = 1:subj
+design(2,i) = i;
+end
+for i = 1:subj
+design(2,subj+i) = i;
+end
+
+cfg.design   = design;
+
+cfg.uvar     = 1;
+cfg.ivar     = 2;
+
+[stat] = ft_freqstatistics(cfg, fast_avg, slow_avg)
+
