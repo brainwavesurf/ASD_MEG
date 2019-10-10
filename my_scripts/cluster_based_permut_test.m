138f55e561a4e5a7a1e9f7e8f25da62178ac3957
Anna Shishkina <shishki.ann@gmail.com>
2019-10-10 14:51:53 +0300

    Cluster-based permutation tests on time-frequency data for within subjects experiments

--- my_scripts/cluster_based_permut_test.m
+++ my_scripts/cluster_based_permut_test.m
@@ -1,17 +1,6 @@
 %% Cluster-based permutation test for within-subjects experiments 
 %(http://www.fieldtriptoolbox.org/tutorial/cluster_permutation_timelock/#within-subjects-experiments)
-% Reading the data for trials in slow and fast conditions
-% Do time-frequency transformation with wavelets for [-1 1.2] time interval
-% Do the grand averages of the TFRs for the fast and slow conditions
-% Do group-level stats for interval of interest [-0.8 0] and frequency range [5 20]
-%==========================================================================
-% error!!!   
-%Error using ft_statfun_depsamplesT (line 84)
-%The data must contain at least two units (usually subjects).
-%Error using ft_statistics_montecarlo (line 244)
-%could not determine the parametric critical value for clustering
-%Error in ft_freqstatistics (line 193)
-%[stat, cfg] = statmethod(cfg, dat, design);
+
 %%
 clear all;
 close all;
@@ -27,8 +16,6 @@ savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
 %%
 %add list of subjects:
 SUBJ = ['0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139'; '0140'; '0141'; '0158'; '0159'; '0160'; '0161'; '0162'; '0163'; '0164'; '0178'; '0179'; '0253'; '0254'; '0255'; '0256'; '0257'; '0259'; '0273'; '0274'; '0275'; '0276'; '0277'; '0346'; '0347'; '0348'; '0350'; '0351'; '0357'; '0358'; '0378'; '0380'; '0381'; '0382'; '0383'; '0384']; 
-%SUBJ = ['0102';'0103'; '0104'];
-
 for s=1: size (SUBJ,1)
     
     subj = SUBJ (s,:); 
