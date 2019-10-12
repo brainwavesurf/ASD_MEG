e3ecb63826681bc196f8d0dd40b3de6114f1fc9c
Anna Shishkina <shishki.ann@gmail.com>
2019-10-12 18:46:10 +0300

    cluster-based permutation test for within subjects experiments for mag and grad

--- my_scripts/cluster_based_permut_test.m
+++ my_scripts/cluster_based_permut_test.m
@@ -1,6 +1,7 @@
 %% Cluster-based permutation test for within-subjects experiments 
 %(http://www.fieldtriptoolbox.org/tutorial/cluster_permutation_timelock/#within-subjects-experiments)
-
+%for magnetometers
+%for gradiometers
 %%
 clear all;
 close all;
@@ -16,6 +17,7 @@ savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
 %%
 %add list of subjects:
 SUBJ = ['0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139'; '0140'; '0141'; '0158'; '0159'; '0160'; '0161'; '0162'; '0163'; '0164'; '0178'; '0179'; '0253'; '0254'; '0255'; '0256'; '0257'; '0259'; '0273'; '0274'; '0275'; '0276'; '0277'; '0346'; '0347'; '0348'; '0350'; '0351'; '0357'; '0358'; '0378'; '0380'; '0381'; '0382'; '0383'; '0384']; 
+
 for s=1: size (SUBJ,1)
     
     subj = SUBJ (s,:); 
@@ -42,7 +44,7 @@ end
 cfg = [];
 cfg.channel          = {'MEGMAG'};
 cfg.latency          = [-0.7 0];
-cfg.frequency        = [8 12];
+cfg.frequency        = 10.5;
 cfg.method           = 'montecarlo';
 cfg.statistic        = 'ft_statfun_depsamplesT';
 cfg.correctm         = 'cluster';
@@ -54,6 +56,7 @@ cfg.clustertail      = 0;
 cfg.alpha            = 0.025;
 cfg.numrandomization = 500;
 % specifies with which sensors other sensors can form clusters
+cfg_neighb = [];
 cfg_neighb.method    = 'distance';
 cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_mag{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
 
@@ -74,6 +77,7 @@ cfg.ivar     = 2;
 
 [within_subj_stat] = ft_freqstatistics(cfg, freqSlow_mag{:}, freqFast_mag{:})
 stat_value = within_subj_stat.stat;
+
 %save stats
 filename = strcat(savepath, '1_results/', 'within_subjs_stat.mat');
 save(filename, 'within_subj_stat');
@@ -82,30 +86,94 @@ filename = strcat(savepath, '1_results/', 'stat_value.mat');
 save(filename, 'stat_value');
    
 %plot
+figure(1)
 cfg = [];
 cfg.alpha  = 0.025;
 cfg.parameter = 'stat';
 cfg.layout = 'neuromag306mag_helmet.mat';
 ft_clusterplot(cfg, within_subj_stat);
-%==========================================================================
-%Error using ft_clusterplot (line 155)
-%this only works if either frequency or time is a singleton dimension
-%here the similar problem described https://mailman.science.ru.nl/pipermail/fieldtrip/2016-July/010752.html
-%==========================================================================
-cfg  = [];
-cfg.avgoverfreq = 'yes';
-stat_avg_freq = ft_selectdata(cfg, within_subj_stat); %stat: [102×1×15 double]
 
+%plot 
+saveas(figure(1),[savepath, '/1_results/', 'cluster-based-permut-test.jpeg']);
+
+%% the same for gradiometers
+
+for s=1: size (SUBJ,1)
+    
+    subj = SUBJ (s,:); 
+    savemegto = strcat(savepath, subj);
+    epofolder = strcat(realdatapath, subj, '/ICA_nonotch_crop', '/epochs/');
+    
+    %load preprocessed epochs
+    epo = load(strcat(epofolder, subj, '_preproc_epochs.mat'));
+    
+    %select mag epochs separately for slow and fast conditions
+    cfg = [];
+    cfg.channel = 'MEGGRAD';
+    epo_fast_grad = ft_selectdata(cfg, epo.fast_epochs);
+    epo_slow_grad = ft_selectdata(cfg, epo.slow_epochs);
+    
+    %load data after freqanalysis 
+    freq_all = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
+    %after mtmconvol
+    freqFast_grad{s}  = freq_all.wvlts_fast_grad;
+    freqSlow_grad{s}  = freq_all.wvlts_slow_grad;
+end
+
+%% do cluster-based permutation test
+cfg = [];
+cfg.channel          = {'MEGGRAD'};
+cfg.latency          = [-0.7 0];
+cfg.frequency        = 10.5;
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan        = 2;
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 500;
+% specifies with which sensors other sensors can form clusters
+cfg_neighb = [];
+cfg_neighb.method    = 'distance';
+cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_grad{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
+
+subj = size (SUBJ,1);
+design = zeros(2,2*subj);
+for i = 1:subj
+design(1,i) = i;
+end
+for i = 1:subj
+design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design   = design;
+cfg.uvar     = 1;
+cfg.ivar     = 2;
+
+[within_subj_stat_grad] = ft_freqstatistics(cfg, freqSlow_grad{:}, freqFast_grad{:})
+stat_value = within_subj_stat.stat;
+
+%save stats
+filename = strcat(savepath, '1_results/', 'within_subjs_stat_grad.mat');
+save(filename, 'within_subj_stat_grad');
+
+filename = strcat(savepath, '1_results/', 'stat_value.mat');
+save(filename, 'stat_value');
+   
+%plot
+figure(2)
 cfg = [];
 cfg.alpha  = 0.025;
 cfg.parameter = 'stat';
-cfg.layout = 'neuromag306mag_helmet.mat';
-ft_clusterplot(cfg, stat_avg_freq);
+cfg.layout = 'neuromag306planar_helmet.mat';%for grad
+ft_clusterplot(cfg, within_subj_stat_grad);
 
-%==========================================================================
-%There are 1 clusters smaller than alpha (0.025)
-%Index exceeds the number of array elements (0).
+%plot 
+saveas(figure(2),[savepath, '/1_results/', 'cluster_based_permut_test_grad.jpeg']);
 
-%Error in ft_clusterplot (line 264)
-%        fprintf('%s%s%s%s%s%s%s%s%s%s%s\n', 'Negative cluster: ',num2str(signeg(iNeg)), ', pvalue: ',num2str(probneg(iNeg)), '
-%        (',hlsignneg(iNeg), ')', ', t = ',num2str(time_perclus(1)), ' to ',num2str(time_perclus(2)))
+
