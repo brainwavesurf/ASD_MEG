49f284007bd718f945034a1431e0e4fd32036301
Anna Shishkina <shishki.ann@gmail.com>
2020-01-30 16:47:44 +0300

    mtmconvol, wavelets and mtmfft freqanalysis

--- my_scripts/freqanalysis.m
+++ my_scripts/freqanalysis.m
@@ -15,7 +15,7 @@ path('/home/a_shishkina/fieldtrip/external/mne/', path);
 realdatapath = '/home/a_shishkina/data/KI/SUBJECTS/';
 savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
 
-%%
+%% load subj info
      
 SUBJ_NT = [ '0101'; '0102'; '0103'; '0104'; '0105'; '0135'; '0136';...  
             '0137'; '0138'; '0140'; '0158'; '0162'; '0163'; '0178';...
@@ -43,9 +43,26 @@ for s=1: size (SUBJ,1)
     epo_fast_mag = ft_selectdata(cfg, epo.fast_epochs);
     epo_slow_mag = ft_selectdata(cfg, epo.slow_epochs);
     
+    cfg.latency = [-0.8 0];
+    epo_fast_mag_pre = ft_selectdata(cfg, epo.fast_epochs);
+    epo_slow_mag_pre = ft_selectdata(cfg, epo.slow_epochs);
+    
+    cfg.latency = [0.4 1.2];
+    epo_fast_mag_post = ft_selectdata(cfg, epo.fast_epochs);
+    epo_slow_mag_post = ft_selectdata(cfg, epo.slow_epochs);
+    
+    cfg = [];
     cfg.channel = 'MEGGRAD';
     epo_fast_grad = ft_selectdata(cfg, epo.fast_epochs);
     epo_slow_grad = ft_selectdata(cfg, epo.slow_epochs);
+    
+    cfg.latency = [-0.8 0];
+    epo_fast_grad_pre = ft_selectdata(cfg, epo.fast_epochs);
+    epo_slow_grad_pre = ft_selectdata(cfg, epo.slow_epochs);
+    
+    cfg.latency = [0.4 1.2];
+    epo_fast_grad_post = ft_selectdata(cfg, epo.fast_epochs);
+    epo_slow_grad_post = ft_selectdata(cfg, epo.slow_epochs);
       
     %% time-frequency response with Hann taper with varying length
     %mtmconvol gives the time-frequency representation of the trial
@@ -79,33 +96,29 @@ for s=1: size (SUBJ,1)
     %% spectral analysis using Hanning taper, for freq < 30 Hz
 
     cfg = [];
-    cfg.method       = 'mtmfft';
-    cfg.output       = 'pow'; 
-    cfg.taper        = 'hanning'; %Hanning taper
-    cfg.keeptrials   = 'yes';
-    cfg.foi          = [5:30];                 
-    cfg.toi          = [-1:0.05:1.2];
-    fft_fast_mag  = ft_freqanalysis(cfg, epo_fast_mag); % trials x Ch x freq
-    fft_slow_mag  = ft_freqanalysis(cfg, epo_slow_mag);
-    fft_fast_grad = ft_freqanalysis(cfg, epo_fast_grad);
-    fft_slow_grad = ft_freqanalysis(cfg, epo_slow_grad);
+    cfg.method         = 'mtmfft';
+    cfg.output         = 'pow'; 
+    cfg.taper          = 'hanning'; %Hanning taper
+    cfg.keeptrials     = 'yes';          
+    cfg.pad            = 10; 
+    cfg.foilim         = [5 30];
+    fft_fast_mag_pre   = ft_freqanalysis(cfg, epo_fast_mag_pre); % trials x Ch x freq
+    fft_slow_mag_pre   = ft_freqanalysis(cfg, epo_slow_mag_pre);
+    fft_fast_mag_post  = ft_freqanalysis(cfg, epo_fast_mag_post); % trials x Ch x freq
+    fft_slow_mag_post  = ft_freqanalysis(cfg, epo_slow_mag_post);
+    
+    fft_fast_grad_pre  = ft_freqanalysis(cfg, epo_fast_grad_pre);
+    fft_slow_grad_pre  = ft_freqanalysis(cfg, epo_slow_grad_pre);
+    fft_fast_grad_post = ft_freqanalysis(cfg, epo_fast_grad_post);
+    fft_slow_grad_post = ft_freqanalysis(cfg, epo_slow_grad_post);
     
     %save freq analysis results
     filename = strcat(savepath, subj, '/', subj, '_freqanalysis.mat');
     save (filename, 'conv_fast_mag', 'conv_slow_mag', 'conv_fast_grad', 'conv_slow_grad', ...
         'wvlts_fast_mag', 'wvlts_slow_mag', 'wvlts_fast_grad', 'wvlts_slow_grad', ...
-        'fft_fast_mag', 'fft_slow_mag', 'fft_fast_grad', 'fft_slow_grad');
+        'fft_fast_mag_pre', 'fft_slow_mag_pre', 'fft_fast_mag_post', 'fft_slow_mag_post', ...
+        'fft_fast_grad_pre', 'fft_slow_grad_pre', 'fft_fast_grad_post', 'fft_slow_grad_post');
 
-    end
-    
-   for s=1: size (SUBJ,1)
-       
-   subj = SUBJ (s,:);    
-   freq_analysis{s} = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
-   
-   end
-   
-   %save stats
-   filename = strcat(savepath, '1_results/', 'freq_analysis.mat');
-   save(filename, 'freq_analysis', '-v7.3');
+end
+
    
