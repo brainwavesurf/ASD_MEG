445a8966cb0fd1fd9ce6c35889bedaf9b5faf8b7
Anna Shishkina <shishki.ann@gmail.com>
2020-04-23 18:15:09 +0300

    cluster analysis in freq domain

--- my_scripts/sensors/cluster_analysis/cluster_based_permut_test.m
+++ my_scripts/sensors/cluster_analysis/cluster_based_permut_test.m
@@ -2,11 +2,8 @@
 %(http://www.fieldtriptoolbox.org/tutorial/cluster_permutation_timelock/#within-subjects-experiments)
 %for magnetometers
 %for gradiometers
-%control, ASD and combined
-%[-0.7 0] to avoid NAN stat
-% avgoverfreq alpha [8 12]
+%all subjects, NT, ASD groups
 %%
-clear all;
 close all;
 
 fieldtripfolder = '/home/a_shishkina/fieldtrip/';
@@ -14,358 +11,138 @@ path(path, fieldtripfolder)
 ft_defaults;
 path('/home/a_shishkina/fieldtrip/external/mne/', path);
 
-realdatapath = '/home/a_shishkina/data/KI/SUBJECTS/';
-savepath = '/home/a_shishkina/data/KI/Results_Alpha_and_Gamma/';
+realdatapath = '/net/server/data/Archive/aut_gamma/orekhova/KI/SUBJECTS/';
+savepath = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/Results_Alpha_and_Gamma/';
 
-%%
-% SUBJ = [ '0076'; '0101'; '0102'; '0103'; '0104'; '0105'; '0106'; '0107'; '0136'; '0137'; '0138'; '0139';...  
-%          '0140'; '0141'; '0158'; '0159'; '0160'; '0161'; '0162'; '0163'; '0164'; '0178'; '0179'; '0253';...
-%          '0254'; '0255'; '0256'; '0257'; '0259'; '0273'; '0274'; '0275'; '0276'; '0346'; '0347'; '0348';... 
-%          '0351'; '0357'; '0358'; '0378'; '0380'; '0381'; '0382'; '0383'; '0384']; 
- 
-     % exclude 0310 (female, researcher, no MRI), 0076(researcher), 0350(researcher/ASD), 0135 (bad
-     % coregistration), 0277 (Female, researcher), 0278/0279 (mom/son-with-epi 'controls'), 349 (ASD/no MRI),
-     % 0379 (NT no MRI), 
-SUBJ_NT = [ '0101'; '0102'; '0103'; '0104'; '0105'; '0136';...  
+% load subj info  
+SUBJ_NT = [ '0101'; '0102'; '0103'; '0104'; '0105'; '0135'; '0136';...  
             '0137'; '0138'; '0140'; '0158'; '0162'; '0163'; '0178';...
-            '0179'; '0255'; '0257'; '0348'; '0378'; '0384']; 
+            '0179'; '0255'; '0257'; '0348'; '0378'; '0379'; '0384']; 
         
 SUBJ_ASD = ['0106'; '0107'; '0139'; '0141'; '0159'; '0160'; '0161';...  
             '0164'; '0253'; '0254'; '0256'; '0273'; '0274'; '0275';...
-            '0276'; '0346'; '0347'; '0351'; '0358';...
-            '0380'; '0381'; '0382'; '0383']; 
-
-% without SUBJ = ['0357'];
+            '0276'; '0346'; '0347'; '0349'; '0351'; '0358';...
+            '0380'; '0381'; '0382'; '0383'];  
         
 SUBJ_ALL = [SUBJ_ASD; SUBJ_NT];
 %% load freqanalysis data for all groups
-for s=1:size(SUBJ,1)
-    
-    subj = SUBJ(s,:); 
-    %load data after freqanalysis 
-    freq_all = load(strcat(savepath, subj, '/', subj, 'wvlts_freqanalysis.mat'));
-    %after mtmconvol
-    freqFast_mag{s} = freq_all.wvlts_fast_mag;
-    freqSlow_mag{s} = freq_all.wvlts_slow_mag;
-    
-end
 
-%% load freqanalysis data for control group
-for s=1:size(SUBJ_NT,1)
-    
-    subj = SUBJ_NT(s,:); 
-    %load data after freqanalysis 
-    freq_all = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
-    %after mtmconvol
-    freqFast_mag_NT{s}  = freq_all.wvlts_fast_mag;
-    freqSlow_mag_NT{s}  = freq_all.wvlts_slow_mag;
+freq_fast_isi = cell(1,size(SUBJ_ALL,1));
+freq_slow_isi = cell(1,size(SUBJ_ALL,1));
+nsub_all = size(SUBJ_ALL,1);
 
-end
-
-%% load freqanalysis data for asd group
-for s=1:size(SUBJ_ASD,1)
-    
-    subj = SUBJ_ASD(s,:); 
-    %load data after freqanalysis 
-    freq_all = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
-    %after mtmconvol
-    freqFast_mag_ASD{s}  = freq_all.wvlts_fast_mag;
-    freqSlow_mag_ASD{s}  = freq_all.wvlts_slow_mag;
-
-end
-%% do cluster-based permutation test for ALL
-
-cfg = [];
-cfg.channel          = {'MEGMAG'};
-cfg.latency          = [-0.7 0];
-cfg.frequency        = [7 13];
-cfg.avgoverfreq      = 'yes';
-cfg.method           = 'montecarlo';
-cfg.statistic        = 'ft_statfun_depsamplesT';
-cfg.correctm         = 'cluster';
-cfg.clusteralpha     = 0.05;
-cfg.clusterstatistic = 'maxsum';
-cfg.minnbchan        = 2;
-cfg.tail             = 0;
-cfg.clustertail      = 0;
-cfg.alpha            = 0.025;
-cfg.numrandomization = 1000;
-% specifies with which sensors other sensors can form clusters
-cfg_neighb = [];
-cfg_neighb.method    = 'distance';
-cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_mag{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
-
-subj = size(SUBJ,1);
-design = zeros(2,2*subj);
-for i = 1:subj
-design(1,i) = i;
-end
-for i = 1:subj
-design(1,subj+i) = i;
-end
-design(2,1:subj)        = 1;
-design(2,subj+1:2*subj) = 2;
-
-cfg.design   = design;
-cfg.uvar     = 1;
-cfg.ivar     = 2;
-
-[within_subj_stat] = ft_freqstatistics(cfg, freqFast_mag{:}, freqSlow_mag{:})
-
-%% do cluster-based permutation test for NT
-
-cfg = [];
-cfg.channel          = {'MEGMAG'};
-cfg.latency          = [-0.7 0];
-cfg.frequency        = [7 13];
-cfg.avgoverfreq      = 'yes';
-cfg.method           = 'montecarlo';
-cfg.statistic        = 'ft_statfun_depsamplesT';
-cfg.correctm         = 'cluster';
-cfg.clusteralpha     = 0.05;
-cfg.clusterstatistic = 'maxsum';
-cfg.minnbchan        = 2;
-cfg.tail             = 0;
-cfg.clustertail      = 0;
-cfg.alpha            = 0.025;
-cfg.numrandomization = 1000;
-% specifies with which sensors other sensors can form clusters
-cfg_neighb = [];
-cfg_neighb.method    = 'distance';
-cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_mag{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
-
-subj = size(SUBJ,1);
-design = zeros(2,2*subj);
-for i = 1:subj
-design(1,i) = i;
-end
-for i = 1:subj
-design(1,subj+i) = i;
-end
-design(2,1:subj)        = 1;
-design(2,subj+1:2*subj) = 2;
-
-cfg.design   = design;
-cfg.uvar     = 1;
-cfg.ivar     = 2;
-
-[within_subj_stat_NT] = ft_freqstatistics(cfg, freqFast_mag{:}, freqSlow_mag{:})
-
-%% do cluster-based permutation test for ASD
-
-cfg = [];
-cfg.channel          = {'MEGMAG'};
-cfg.latency          = [-0.7 0];
-cfg.frequency        = [7 13];
-cfg.avgoverfreq      = 'yes';
-cfg.method           = 'montecarlo';
-cfg.statistic        = 'ft_statfun_depsamplesT';
-cfg.correctm         = 'cluster';
-cfg.clusteralpha     = 0.05;
-cfg.clusterstatistic = 'maxsum';
-cfg.minnbchan        = 2;
-cfg.tail             = 0;
-cfg.clustertail      = 0;
-cfg.alpha            = 0.025;
-cfg.numrandomization = 1000;
-% specifies with which sensors other sensors can form clusters
-cfg_neighb = [];
-cfg_neighb.method    = 'distance';
-cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_mag_ASD{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
-
-subj = size(SUBJ_ASD,1);
-design = zeros(2,2*subj);
-for i = 1:subj
-design(1,i) = i;
-end
-for i = 1:subj
-design(1,subj+i) = i;
-end
-design(2,1:subj)        = 1;
-design(2,subj+1:2*subj) = 2;
-
-cfg.design   = design;
-cfg.uvar     = 1;
-cfg.ivar     = 2;
-[within_subj_stat_ASD] = ft_freqstatistics(cfg, freqFast_mag_ASD{:}, freqSlow_mag_ASD{:})
-
-%% plot
-cfg = [];
-cfg.alpha  = 0.025;
-cfg.parameter = 'stat';
-cfg.layout = 'neuromag306mag.lay';
-
-ft_clusterplot(cfg, within_subj_stat); %nothing to plot
-ft_clusterplot(cfg, within_subj_stat_NT); %4 clusters
-ft_clusterplot(cfg, within_subj_stat_ASD); %nothing to plot
-
-%plot 
-saveas(figure(2),[savepath, '/1_results/', 'cluster_based_control_avg7_13.jpeg']);
-saveas(figure(1),[savepath, '/1_results/', 'cluster_based_combined_avg10_13.jpeg']);
-
-%% the same for gradiometers
-% load freqanalysis data for all groups
 for s=1:size(SUBJ_ALL,1)
     
     subj = SUBJ_ALL(s,:); 
     %load data after freqanalysis 
-    freq_all = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
+    load(strcat(savepath, subj, '/', subj, '_alpha_freqanalysis.mat'));
     %after mtmconvol
-    freqFast_grad{s}  = freq_all.wvlts_fast_grad;
-    freqSlow_grad{s}  = freq_all.wvlts_slow_grad;
+    freq_fast_isi{s} = fft_fast_isi;
+    freq_slow_isi{s} = fft_slow_isi;
 end
 
-% load freqanalysis data for control group
+
+%% load freqanalysis data for control group
+freq_fast_isi_NT = cell(1,size(SUBJ_NT,1));
+freq_slow_isi_NT = cell(1,size(SUBJ_NT,1));
+nsub_NT = size(SUBJ_NT,1);
 for s=1:size(SUBJ_NT,1)
     
     subj = SUBJ_NT(s,:); 
     %load data after freqanalysis 
-    freq_all = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
+    load(strcat(savepath, subj, '/', subj, '_alpha_freqanalysis.mat'));
     %after mtmconvol
-    freqFast_grad_NT{s}  = freq_all.wvlts_fast_grad;
-    freqSlow_grad_NT{s}  = freq_all.wvlts_slow_grad;
+    freq_fast_isi_NT{s} = fft_fast_isi;
+    freq_slow_isi_NT{s} = fft_slow_isi;
 end
 
-% load freqanalysis data for asd group
+%% load freqanalysis data for asd group
+freq_fast_isi_ASD = cell(1,size(SUBJ_ASD,1));
+freq_slow_isi_ASD = cell(1,size(SUBJ_ASD,1));
+nsub_ASD = size(SUBJ_ASD,1);
 for s=1:size(SUBJ_ASD,1)
     
     subj = SUBJ_ASD(s,:); 
     %load data after freqanalysis 
-    freq_all = load(strcat(savepath, subj, '/', subj, '_freqanalysis.mat'));
+    load(strcat(savepath, subj, '/', subj, '_alpha_freqanalysis.mat'));
     %after mtmconvol
-    freqFast_grad_ASD{s}  = freq_all.wvlts_fast_grad;
-    freqSlow_grad_ASD{s}  = freq_all.wvlts_slow_grad;
+    freq_fast_isi_ASD{s} = fft_fast_isi;
+    freq_slow_isi_ASD{s} = fft_slow_isi;
 end
 
-%% do cluster-based permutation test
+%mag or grad
+channels     = 'megmag'; % 'megplanar';
+layout1      = 'neuromag306mag_helmet.mat'; %'neuromag306planar_helmet.mat';
+layout2      = 'neuromag306mag.lay'; %'neuromag306planar.lay'; 
+template     = 'neuromag306mag_neighb.mat';  % neuromag306planar_neighb.mat'; 
+%% do cluster-based permutation test for ALL
 cfg = [];
-cfg.channel          = {'MEGGRAD'};
-cfg.latency          = [-0.7 0];
-cfg.frequency        = [10 13];
-cfg.avgoverfreq      = 'yes';
-cfg.method           = 'montecarlo';
-cfg.statistic        = 'ft_statfun_depsamplesT';
-cfg.correctm         = 'cluster';
-cfg.clusteralpha     = 0.05;
-cfg.clusterstatistic = 'maxsum';
-cfg.minnbchan        = 2;
-cfg.tail             = 0;
-cfg.clustertail      = 0;
-cfg.alpha            = 0.025;
-cfg.numrandomization = 1000;
-% specifies with which sensors other sensors can form clusters
-cfg_neighb = [];
-cfg_neighb.method    = 'distance';
-cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_grad{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
+cfg.method      = 'template'; 
+cfg.template    = template;              
+cfg.layout      = layout2;             
+cfg.feedback    = 'yes';                             
+neighbours      = ft_prepare_neighbours(cfg, freq_fast_isi{1});
+neighbours_NT      = ft_prepare_neighbours(cfg, freq_fast_isi_NT{1});
+neighbours_ASD      = ft_prepare_neighbours(cfg, freq_fast_isi_ASD{1});
 
-subj = size (SUBJ_ALL,1);
-design = zeros(2,2*subj);
-for i = 1:subj
-design(1,i) = i;
-end
-for i = 1:subj
-design(1,subj+i) = i;
-end
-design(2,1:subj)        = 1;
-design(2,subj+1:2*subj) = 2;
+cfg=[];
+cfg.channel     = channels; 
+cfg.parameter   = 'powspctrm';
+cfg.method      = 'montecarlo';
+cfg.statistic   = 'ft_statfun_depsamplesT';
+cfg.alpha       = 0.05;
+cfg.minnbchan   = 1;
+cfg.correctm    = 'cluster'; %'fdr'
+cfg.correcttail = 0.025;
+cfg.numrandomization = 10000; 
+cfg.clusterstatistic = 'maxsum'; 
+cfg.clusterthreshold = 'parametric'; 
+cfg.clusteralpha     = 0.05; 
+cfg.clustertail      = 0; 
 
-cfg.design   = design;
-cfg.uvar     = 1;
-cfg.ivar     = 2;
+cfg.design(1,1:2*nsub_all)  = [ones(1,nsub_all) 2*ones(1,nsub_all)];
+cfg.design(2,1:2*nsub_all)  = [1:nsub_all 1:nsub_all];
+cfg.ivar                = 1; 
+cfg.uvar                = 2; 
+cfg.neighbours  = neighbours;
+[stat_all] = ft_freqstatistics(cfg, freq_fast_isi{:}, freq_slow_isi{:});
 
-[within_subj_stat_grad] = ft_freqstatistics(cfg, freqFast_grad{:}, freqSlow_grad{:})
+cfg.design(1,1:2*nsub_NT)  = [ones(1,nsub_NT) 2*ones(1,nsub_NT)];
+cfg.design(2,1:2*nsub_NT)  = [1:nsub_NT 1:nsub_NT];
+cfg.ivar                = 1; 
+cfg.uvar                = 2; 
+cfg.neighbours  = neighbours_NT;
+[stat_NT] = ft_freqstatistics(cfg, freq_fast_isi_NT{:}, freq_slow_isi_NT{:});
 
-%% do cluster-based permutation test for NT
-cfg = [];
-cfg.channel          = {'MEGGRAD'};
-cfg.latency          = [-0.7 0];
-cfg.frequency        = [10 13];
-cfg.avgoverfreq      = 'yes';
-cfg.method           = 'montecarlo';
-cfg.statistic        = 'ft_statfun_depsamplesT';
-cfg.correctm         = 'cluster';
-cfg.clusteralpha     = 0.05;
-cfg.clusterstatistic = 'maxsum';
-cfg.minnbchan        = 2;
-cfg.tail             = 0;
-cfg.clustertail      = 0;
-cfg.alpha            = 0.025;
-cfg.numrandomization = 1000;
-% specifies with which sensors other sensors can form clusters
-cfg_neighb = [];
-cfg_neighb.method    = 'distance';
-cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_grad_NT{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
-
-subj = size (SUBJ_NT,1);
-design = zeros(2,2*subj);
-for i = 1:subj
-design(1,i) = i;
-end
-for i = 1:subj
-design(1,subj+i) = i;
-end
-design(2,1:subj)        = 1;
-design(2,subj+1:2*subj) = 2;
-
-cfg.design   = design;
-cfg.uvar     = 1;
-cfg.ivar     = 2;
-
-[within_subj_stat_grad_NT] = ft_freqstatistics(cfg, freqFast_grad_NT{:}, freqSlow_grad_NT{:})
-
-%% do cluster-based permutation test for ASD
-cfg = [];
-cfg.channel          = {'MEGGRAD'};
-cfg.latency          = [-0.7 0];
-cfg.frequency        = [10 13];
-cfg.avgoverfreq      = 'yes';
-cfg.method           = 'montecarlo';
-cfg.statistic        = 'ft_statfun_depsamplesT';
-cfg.correctm         = 'cluster';
-cfg.clusteralpha     = 0.05;
-cfg.clusterstatistic = 'maxsum';
-cfg.minnbchan        = 2;
-cfg.tail             = 0;
-cfg.clustertail      = 0;
-cfg.alpha            = 0.025;
-cfg.numrandomization = 1000;
-% specifies with which sensors other sensors can form clusters
-cfg_neighb = [];
-cfg_neighb.method    = 'distance';
-cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, freqFast_grad_ASD{1}); %using a distance threshold of 4, there are on average 13.8 neighbours per channel
-
-subj = size (SUBJ_ASD,1);
-design = zeros(2,2*subj);
-for i = 1:subj
-design(1,i) = i;
-end
-for i = 1:subj
-design(1,subj+i) = i;
-end
-design(2,1:subj)        = 1;
-design(2,subj+1:2*subj) = 2;
-
-cfg.design   = design;
-cfg.uvar     = 1;
-cfg.ivar     = 2;
-
-[within_subj_stat_grad_ASD] = ft_freqstatistics(cfg, freqFast_grad_ASD{:}, freqSlow_grad_ASD{:})
-
+cfg.design(1,1:2*nsub_ASD)  = [ones(1,nsub_ASD) 2*ones(1,nsub_ASD)];
+cfg.design(2,1:2*nsub_ASD)  = [1:nsub_ASD 1:nsub_ASD];
+cfg.ivar                = 1; 
+cfg.uvar                = 2; 
+cfg.neighbours  = neighbours_ASD;
+[stat_ASD] = ft_freqstatistics(cfg, freq_fast_isi_ASD{:}, freq_slow_isi_ASD{:});
 
 %save stats
-filename = strcat(savepath, '1_results/', 'cluster_based_stat.mat');
-save(filename, 'within_subj_stat', 'within_subj_stat_NT', 'within_subj_stat_ASD', 'within_subj_stat_grad', 'within_subj_stat_grad_NT', 'within_subj_stat_grad_ASD');
+filename = strcat(savepath, '1_results/sensor_clusters/', 'cluster_based_stat_mag.mat');
+save(filename, 'stat_all', 'stat_NT', 'stat_ASD')
 
-   
+% filename = strcat(savepath, '1_results/', 'cluster_based_stat_grad.mat');
+% save(filename, 'stat_all', 'stat_NT', 'stat_ASD')
+
 %plot
 cfg = [];
 cfg.alpha  = 0.025;
 cfg.parameter = 'stat';
-cfg.layout = 'neuromag306planar.lay';%for grad
-ft_clusterplot(cfg, within_subj_stat_grad); %15 clusters
-ft_clusterplot(cfg, within_subj_stat_grad_NT); %6 clusters
-ft_clusterplot(cfg, within_subj_stat_grad_ASD); %nothing to plot
+cfg.layout = layout2;
+ft_clusterplot(cfg, stat_all); 
+ft_clusterplot(cfg, stat_NT); 
+ft_clusterplot(cfg, stat_ASD); 
+
 %plot 
-saveas(figure(1),[savepath, '/1_results/', 'cluster_based_grad_combined_avg10_13.jpeg']);
-saveas(figure(2),[savepath, '/1_results/', 'cluster_based_grad_control_avg10_13.jpeg']);
-saveas(figure(3),[savepath, '/1_results/', 'cluster_based_grad_ASD_avg10_13.jpeg']);
+saveas(figure(1),[savepath, '/1_results/sensor_clusters/', 'cluster_all_mag.jpeg']);
+saveas(figure(2),[savepath, '/1_results/sensor_clusters/', 'cluster_NT_mag.jpeg']);
+saveas(figure(3),[savepath, '/1_results/sensor_clusters/', 'cluster_ASD_mag.jpeg']);
+
+% saveas(figure(1),[savepath, '/1_results/sensor_clusters/', 'cluster_all_grad.jpeg']);
+% saveas(figure(2),[savepath, '/1_results/sensor_clusters/', 'cluster_NT_grad.jpeg']);
+% saveas(figure(3),[savepath, '/1_results/sensor_clusters/', 'cluster_ASD_grad.jpeg']);
+
